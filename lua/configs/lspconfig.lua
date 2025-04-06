de9a81ac787ee9234f547dbf5f12cc604efa0623
thomasisebeck <tisebeck@proton.me>
2025-04-06 20:50:46 +0200

    remove lua ls from default config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "lua-language-server", "clangd" }
+local servers = { "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
