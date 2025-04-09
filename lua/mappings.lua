516cef35d0d7bed833ae81e4ba555b32fa8d1ca4
ThomasIsebeck <tisebeck@proton.me>
2025-04-09 21:51:20 +0200

    prev buffer

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>n", ":bn<CR>", { desc = "Go to the next buffer" })
+map("n", "<leader>p", ":bp<CR>", { desc = "Go to the previous buffer" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
