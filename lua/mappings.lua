a0f2aef489f174b56823b659cd2e81604793f929
ThomasIsebeck <tisebeck@proton.me>
2025-04-09 19:14:54 +0200

    go to next buffer with n

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>n", ":n<CR>", { desc = "Go to the next buffer" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
