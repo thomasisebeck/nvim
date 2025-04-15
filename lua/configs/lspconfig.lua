9f32cde8361ce343404b054585f514ad3716dac9
ThomasIsebeck <tisebeck@proton.me>
2025-04-15 19:27:54 +0200

    added tailwind

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "clangd", "ts_ls" }
+local servers = { "clangd", "ts_ls", "tailwindcss-language-server" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,15 +29,4 @@ return {
       },
     },
   },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        javascript = { "eslint" },
-        javascriptreact = { "eslint" },
-        typescript = { "eslint" },
-        typescriptreact = { "eslint" },
-      },
-    },
-  },
 }
