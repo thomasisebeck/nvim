e2416e9e13e08648026e736e9a1d1a65da5c0ef7
ThomasUvirco <thomas@uvirco.com>
2025-05-27 12:05:26 +0000

    add css lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "clangd", "ts_ls", "tailwindcss", "eslint", "lua_ls", "pyright" }
+local servers = { "clangd", "ts_ls", "tailwindcss", "eslint", "lua_ls", "pyright", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
