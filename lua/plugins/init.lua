09f453d5c69c557c1b9c2c2d764f6f09497a8c9a
ThomasIsebeck <tisebeck@proton.me>
2025-06-09 20:44:47 +0200

    flutter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,63 @@
 return {
+  ------ flutter and dart -----
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = function()
+      require("flutter-tools").setup {
+
+        debugger = {
+          -- make these two params true to enable debug mode
+          enabled = false,
+          run_via_dap = false,
+          register_configurations = function(_)
+            require("dap").adapters.dart = {
+              type = "executable",
+              command = vim.fn.stdpath "data" .. "/mason/bin/dart-debug-adapter",
+              args = { "flutter" },
+            }
+
+            require("dap").configurations.dart = {
+              {
+                type = "dart",
+
+                request = "launch",
+
+                name = "Launch flutter",
+                dartSdkPath = "home/flutter/bin/cache/dart-sdk/",
+                flutterSdkPath = "home/flutter",
+                program = "${workspaceFolder}/lib/main.dart",
+                cwd = "${workspaceFolder}",
+              },
+            }
+          end,
+        },
+
+        dev_log = {
+
+          -- toggle it when you run without DAP
+          enabled = false,
+          open_cmd = "tabedit",
+        },
+        lsp = {
+
+          on_attach = require("lvim.lsp").common_on_attach,
+          capabilities = require("lvim.lsp").default_capabilities,
+        },
+      }
+    end,
+  },
+
+  {
+    "dart-lang/dart-vim-plugin",
+  },
+
+  ----------
+
+  { "mfussenegger/nvim-dap" },
   {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
@@ -36,7 +95,7 @@ return {
       },
     },
   },
-    {
+  {
     "tronikelis/conflict-marker.nvim",
     config = function()
       require("conflict-marker").setup {
@@ -72,7 +131,7 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-   lazy = false,
+    lazy = false,
     opts = {
       keywords = {
         TEST = { icon = "⏲ ", color = "#b392e9", alt = { "TESTING" } },
