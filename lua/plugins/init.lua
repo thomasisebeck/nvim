465ae5aaa57d8f6c165df98637dcd68885253d64
Thomas Isebeck <tisebeck@proton.me>
2025-11-02 17:07:44 +0200

    add prettier

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,59 +27,6 @@ return {
     end,
   },
 
-  -- flutter stuff
-  {
-    "akinsho/flutter-tools.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim",
-    },
-    config = function()
-      require("flutter-tools").setup {
-
-        debugger = {
-          -- make these two params true to enable debug mode
-          enabled = false,
-          run_via_dap = false,
-          register_configurations = function(_)
-            require("dap").adapters.dart = {
-              type = "executable",
-              command = vim.fn.stdpath "data" .. "/mason/bin/dart-debug-adapter",
-              args = { "flutter" },
-            }
-
-            require("dap").configurations.dart = {
-              {
-                type = "dart",
-                request = "launch",
-                name = "Launch flutter",
-                dartSdkPath = "home/flutter/bin/cache/dart-sdk/",
-                flutterSdkPath = "home/flutter",
-                program = "${workspaceFolder}/lib/main.dart",
-                cwd = "${workspaceFolder}",
-              },
-            }
-          end,
-        },
-        dev_log = {
-          -- toggle it when you run without DAP
-          enabled = false,
-          open_cmd = "tabedit",
-        },
-        lsp = {
-          on_attach = require("lvim.lsp").common_on_attach,
-          capabilities = require("lvim.lsp").default_capabilities,
-        },
-      }
-    end,
-  },
-
-  {
-    "dart-lang/dart-vim-plugin",
-  },
-
-  ----------
-
   { "mfussenegger/nvim-dap" },
   {
     "folke/trouble.nvim",
@@ -207,6 +154,11 @@ return {
     opts = function()
       require "configs.none-ls"
     end,
+  },
+  {
+    "prettier/vim-prettier",
+    run = "yarn install --frozen-lockfile --production",
+    ft = { "javascript", "typescript", "css", "scss", "json", "graphql", "markdown", "vue", "yaml", "html" },
   },
   {
     "windwp/nvim-ts-autotag",
