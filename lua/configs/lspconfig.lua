54a2b9b29e89f4de222a417c9b098192bf3563b8
thomasisebeck <tisebeck@proton.me>
2025-12-16 21:12:49 +0200

    fix config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,27 +1,8 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+require "lspconfig"
 
 local servers = { "clangd", "ts_ls", "tailwindcss", "eslint", "lua_ls", "pyright", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- add vim as a global to lua
-lspconfig["lua_ls"].setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
-}
+vim.lsp.enable(servers)
